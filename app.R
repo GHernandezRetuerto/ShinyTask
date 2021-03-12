5d3044fc81e1f13c30ed5de05f63bc82d33c9f1c
Guillermo Hernández <g.hernandezretuerto@gmail.com>
2021-03-12 22:22:32 +0100

    Uploaded data from UCI repository.

--- app.R
+++ app.R
@@ -1,6 +1,12 @@
+require(shiny)
+require(tidyverse)
+require(bslib)
+require(ggplot2)
+require(plotly)
+require(thematic)
 
-
-library(shiny)
+data =  read.csv(url('https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'))
+head(data)
 
 # Define UI for application that draws a histogram
 ui = fluidPage(
