a38a6d247e059edc1421f873a88027a4fea4f9f7
Guillermo Hernández <g.hernandezretuerto@gmail.com>
2021-03-13 23:46:09 +0100

    Switchs changed for checkbox. Further data processing (elimination of missing values, factor rearrangement...)

--- app.R
+++ app.R
@@ -1,18 +1,27 @@
 require(shiny)
-require(tidyverse)
-require(bslib)
-require(ggplot2)
-require(plotly)
+#theming
+require(bslib) 
 require(thematic)
+#data handling
+require(tidyverse)
+#plotting
+require(ggplot2)
+library(ggpubr)
+require(plotly)
+#shiny misc
 require(DT)
 require(shinyWidgets)
 
-data =  read.csv(url('https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'), header=FALSE, stringsAsFactors = TRUE)
+data =  read.csv(url('https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'), header=FALSE, stringsAsFactors = TRUE, sep=',')
 colnames(data) = c('age','workclass','fnlwgt','education','education_num','marital_status','occupation','relationship','race','sex','cap_gain','cap_loss','hours_week','native_country','label')
 
 # Some cleaning and selection of variables
 data = data[complete.cases(data), ] %>% # Only data without NA
-    select(age, workclass, education, race, sex, hours_week, label)
+    select(age, workclass, education, race, sex, hours_week, label) %>%
+    na.omit()
+
+# Eliminating rows with ' ?' valuesstr
+data = data[rowSums(data == " ?") == 0, , drop = FALSE]
 
 # Checking the presence of NAs
 NAcount = function(vector){
@@ -24,6 +33,31 @@ if (sum(sapply(data, NAcount)) != 0){cat('ERROR: NAs detected.')}
 library(caret)
 part = createDataPartition(data$label, p=0.3, list=FALSE)
 data = data[part,]
+
+#Modification of 'workclass'
+levels(data$workclass) = c(levels(data$workclass), "Public", "Self Employed")
+data$workclass = gsub('.*^\\ Self.*', 'Self Employed', data$workclass)
+data$workclass = gsub('.*\\gov$.*', 'Public', data$workclass)
+data$workclass = gsub('.*\\pay$.*', 'Without Pay', data$workclass)
+data$workclass = factor(data$workclass, levels = c(' Private', 'Public', 'Self Employed', 'Without Pay'))
+
+#Modification of 'education'
+levels(data$education) = c(levels(data$education), "Elementary school", "Middle School", "High School",'Unfinnished College')
+data$education = gsub(' 1st-4th', 'Elementary School', data$education)
+data$education = gsub(' 5th-6th', 'Middle School', data$education)
+data$education = gsub(' 7th-8th', 'Middle School', data$education)
+data$education = gsub(' 9th', 'High School', data$education)
+data$education = gsub(' 10th', 'High School', data$education)
+data$education = gsub(' 11th', 'High School', data$education)
+data$education = gsub(' 12th', 'High School', data$education)
+data$education = gsub(' Some-college', 'Unfinnished College', data$education)
+data$education = gsub(' Assoc-voc', 'Assoc', data$education)
+data$education = gsub(' Assoc-acdm', 'Assoc', data$education)
+data$education = factor(data$education)
+data$education = ordered(data$education, levels = c(' Preschool','Elementary School','Middle School','High School',' HS-grad',' Prof-school','Assoc','Unfinnished College',' Bachelors',' Masters',' Doctorate'))  
+
+label_choice = unique(data$label)
+
 
 # Define UI for application that draws a histogram
 # Define UI
@@ -58,8 +92,15 @@ ui = navbarPage(
     tabPanel("The Variables",
              fluidPage(
                  p('Check yourself the data in the nest interactive charts.'),
-                 prettySwitch(inputId = 'label0', label='  Show <50K Observations'),
-                 prettySwitch(inputId = 'label1', label='  Show >50K Observations'),
+                 awesomeCheckboxGroup(
+                     inputId = 'label',
+                     label = 'Select the populations to show',
+                     choices = label_choice,
+                     selected = label_choice,
+                     inline = FALSE,
+                     status = "primary",
+                     width = NULL
+                 ),
                  tabsetPanel(
                      tabPanel('Age',
                          sidebarLayout(
